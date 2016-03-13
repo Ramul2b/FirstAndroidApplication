b2d9950722acbe614712fe36d59296a8824171d2
Ramul2B <ramul2b@mail.ru>
2016-03-13 21:01:56 +0300

    Function of transmitting an empty string in table was realized.

--- src/com/firstandroidapplication/MainActivity.java
+++ src/com/firstandroidapplication/MainActivity.java
@@ -7,56 +7,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	TextView	urlConnect;
-	TextView	urlAddres;
+	TextView urlConnect;
+	TextView urlAddres;
+	Button createNew;
+	TableLayout urlTable;
+	TableRow tr;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		// TextView urlConnect = (TextView) findViewById(R.id.urlConnect);
-		// TextView urlAddres = (TextView) findViewById(R.id.urlAddres);
-
-	}
-
-	public void urlAddRow(String addresOfUrl, String connect) {
-
-		TableLayout urlTable = (TableLayout) findViewById(R.id.urlTable);
-		LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		
-		TableRow tr = (TableRow) inflater.inflate(R.layout.urlstring, null);
+		createNew = (Button) findViewById(R.id.createNew);
 		
-		TextView tv = (TextView) tr.getChildAt(0);		
-		tv.setText(addresOfUrl);
-		tv = (TextView) tr.getChildAt(1);
-		tv.setText(connect);
-		urlTable.addView(tr);
+		urlTable = (TableLayout) findViewById(R.id.urlTable);		
+		
+		OnClickListener btnNewRow = new OnClickListener() {
+			@Override
+			public void onClick(View v){
+				
+				LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				tr = (TableRow) inflater.inflate(R.layout.urlstring, null);
+				urlTable.addView(tr);
+				
+			}
+		};
+
+		createNew.setOnClickListener(btnNewRow);
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		return super.onCreateOptionsMenu(menu);
-	}
-
-	@Override
-	public boolean onPrepareOptionsMenu(Menu menu) {
-
-		return super.onPrepareOptionsMenu(menu);
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-
-		return super.onOptionsItemSelected(item);
-	}
 }
